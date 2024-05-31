cf9d9b7205c23746ccdfd34e23d7713d9dbf7487
BIGJeon2 <robert1157311@gmail.com>
2024-05-31 18:25:25 +0900

    before injection company set

--- app/src/main/java/com/jeon/daggerpractice/Beverage.kt
+++ app/src/main/java/com/jeon/daggerpractice/Beverage.kt
@@ -3,5 +3,4 @@ package com.jeon.daggerpractice
 interface Beverage {
     val beverageInfo: BeverageInfo
     fun showInfo()
-    fun showCompany()
 }
--- app/src/main/java/com/jeon/daggerpractice/model/Milk.kt
+++ app/src/main/java/com/jeon/daggerpractice/model/Milk.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import android.widget.Toast
 import com.jeon.daggerpractice.Beverage
 import com.jeon.daggerpractice.BeverageInfo
+import javax.inject.Inject
 
 class Milk(context: Context, override val beverageInfo: BeverageInfo) : Beverage {
     private val mContext = context
+    @Inject
+    lateinit var company: String
     override fun showInfo() {
         Toast.makeText(mContext, "This is Milk", Toast.LENGTH_SHORT).show()
     }
     override fun showCompany() {
-        Toast.makeText(mContext, "This is Compose beverage", Toast.LENGTH_SHORT).show()
+        Toast.makeText(mContext, "This is ${company} beverage", Toast.LENGTH_SHORT).show()
     }
 }
